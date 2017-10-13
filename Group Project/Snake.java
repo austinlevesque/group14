fcf2c323372c29a7492c88cc14d2d15441fe674c
Austin Levesque <32713540+austinlevesque@users.noreply.github.com>
2017-10-13 17:40:41 -0600

    Create Snake.java

---
+++ Group Project/Snake.java
@@ -0,0 +1,8 @@
+public class Snake{
+
+    public static void main(String[] args){
+        Grid newGrid = new Grid();                
+        newGrid.printGrid();
+        newGrid.userInput();  
+    }
+}
