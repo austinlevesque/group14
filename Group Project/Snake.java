f8d55237e76d5d4e0c55af7978dca96f0082f39a
Rotciv8 <victorchu1996@gmail.com>
2017-10-13 20:14:02 -0600

    Update Snake.java

--- Group Project/Snake.java
+++ Group Project/Snake.java
@@ -1,8 +1,22 @@
-public class Snake{
+/**
+ * Main Class that runs the game.
+ *
+ * @Victor
+ * @Steven
+ * @Chris
+ * @Austin
+ * @Nav
+ */
+ 
+public class SnakeMain {
 
     public static void main(String[] args){
-        Grid newGrid = new Grid();                
+        Grid newGrid = new Grid();
         newGrid.printGrid();
-        newGrid.userInput();  
+        newGrid.userInput();
+        newGrid.inputValid();
+        newGrid.moveSnake();
+        newGrid.printGrid();
     }
 }
+
