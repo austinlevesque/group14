4aaa7f85451190556d73f525a3cfa87cc8ef3dad
victorchu8 <victorchu1996@gmail.com>
2017-10-28 16:03:59 -0600

    Add files via upload

--- Group Project/Grid.java
+++ Group Project/Grid.java
@@ -1,213 +1,77 @@
-
-/**
- * Main Operating Class.
- */
-
-import java.util.Scanner;
 import java.util.Random;
 
-public class Grid {
+public class Grid{
 
-  int x = 9;
-  int y = 9;
-  int foodX = 5;
-  int foodY = 5;
-  String input;
-  int yLength = 10;
-  int xLength = 10;
-  int score;
+	int score;
+	Snake mainSnake;
 
-/**
- * Prints the Grid for the Snake to move on.
- *
- * @return void
- */
+	//Constructors
+	public Grid(){
+	}
 
-public int gameScore() {
-  if (checkFood() == false) {
-    setScore(getScore()+1);
-  }
-  return getScore();
-}
+	public Grid(Snake main,int scr){
+		mainSnake = main;
+		score = scr;
+	}
 
-public void printGrid() {
-  System.out.println("Game Score: " + getScore());
-  String[][] arrayGrid = new String[getYlength()][getXlength()];
-  for(int row = 0; row < getXlength(); row++) {
-    for(int column = 0; column < getYlength(); column++) {
-        arrayGrid[row][column] = ". ";
-        arrayGrid[getFoodY()][getFoodX()] = "* ";
-        arrayGrid[getY()][getX()] = "S ";
-        System.out.print(arrayGrid[row][column]);
-    }
-    System.out.println();
-  }
-}
+	/**
+	* Prints the Grid for the Snake to move on.
+	*
+	* Grid generation taken from  http://www.dreamincode.net/forums/topic/233382-2-d-array-grid-design/
+	* @return void
+	*/
+	public void printGrid(){
+	  System.out.println("Game Score: " + getScore());
+	  String[][] arrayGrid = new String[mainSnake.getYlength()][mainSnake.getXlength()];
+	  for(int row = 0; row < mainSnake.getXlength(); row++){
+		for(int column = 0; column < mainSnake.getYlength(); column++){
+			arrayGrid[row][column] = ". ";
+			arrayGrid[mainSnake.getFoodY()][mainSnake.getFoodX()] = "* ";
+			arrayGrid[mainSnake.getY()][mainSnake.getX()] = "S ";
+			for(int j = mainSnake.tailX.size(); j>0; j--){
+			//draws the tail and detects if the head has collided with the tail
+				if(arrayGrid[(mainSnake.tailY.get(j-1))][mainSnake.tailX.get(j-1)] == "S "){
+				//if the head has collided with the tail
+					mainSnake.gameover = true;
+				}else{
+					arrayGrid[(mainSnake.tailY.get(j-1))][mainSnake.tailX.get(j-1)] = "s ";
+				}
+			}
+			System.out.print(arrayGrid[row][column]);
+		}
+		System.out.println();
+	  }
+	}
 
+	/**
+	* Generates random coordinates for the food.
+	*
+	* @return int
+	*/
 
-/**
- * Generates random coordinates for the food.
- *
- * @return int
- */
+	public void generateFood(){
+	  while (checkFood() == false){
+		Random randX = new Random();
+		Random randY = new Random();
+		mainSnake.setFoodX(randX.nextInt(mainSnake.xLength));
+		mainSnake.setFoodY(randY.nextInt(mainSnake.yLength));
+	  }
+	}
 
-public void generateFood() {
+	public boolean checkFood(){
+		boolean flag = true;
+		if (mainSnake.getFoodY() == mainSnake.getY() && mainSnake.getFoodX() == mainSnake.getX()){
+			flag = false;
+		}
+		return flag;
+	}
 
-  while (checkFood() == false) {
-    Random randX = new Random();
-    Random randY = new Random();
-    setFoodX(randX.nextInt(xLength));
-    setFoodY(randY.nextInt(yLength));
-  }
-}
+	public int getScore(){
+	  	  return score;
+  	}
 
-/**
- * Checks for food on grid.
- *
- * @return boolean
- */
+  	public void setScore(int scoreToSet){
+  	  score = scoreToSet;
+  	}
 
- public boolean checkFood() {
-   boolean flag = true;
-    if (getFoodY() == getY() && getFoodX() == getX()) {
-      flag = false;
-    }
-    return flag;
- }
-
- /**
-  * Generates more "s" body parts when it eats "*"
-  *
-  * @return
-  */
-
- public void generateBody() {
-   if (checkFood() == false) {
-   }
- }
-
-/**
- * Prompts user for movement of the snake in any direction according to "a", "s", "w" and "d".
- * Checks if user input is a valid key for movement.
- *
- * @return boolean
- */
-
-public boolean userInput() {
-    boolean valid = false;
-    while (valid == false) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("movement: ");
-        setInput(keyboard.nextLine());
-        if (input.equals("w") || input.equals("a") ||input.equals("s") ||input.equals("d")) {
-            valid = true;
-        }
-        else {
-        System.out.println("false");
-        }
-      }
-      return valid;
-}
-
-/**
- * Takes key input and checks which way for snake to move.
- *
- * @return void
- */
-
-public void moveSnake() {
-    int moveY = getY();
-    int moveX = getX();
-
-    if (input.equals("w")) {
-      moveY--;
-        if (moveY < 0) {
-          moveY = getYlength() - 1;
-        }
-      setY(moveY);
-    }
-    else if (input.equals("s")) {
-      moveY++;
-        if (moveY > getYlength() - 1) {
-          moveY = getYlength() - getYlength();
-        }
-      setY(moveY);
-    }
-    else if (input.equals("a")) {
-      moveX--;
-        if (moveX < 0) {
-          moveX = getXlength() - 1;
-        }
-      setX(moveX);
-    }
-    else if (input.equals("d")) {
-      moveX++;
-        if (moveX > getXlength() - 1) {
-          moveX = getXlength() - getXlength();
-        }
-      setX(moveX);
-    }
-}
-
-/**
- * getter/setter methods
- *
- * @return int
- * @return void
- */
-
-public int getX() {
-    return x;
-}
-
-public int getY() {
-    return y;
-}
-
-public void setX(int sx) {
-    x = sx;
-}
-
-public void setY(int sy) {
-    y = sy;
-}
-
-public String getInput() {
-  return input;
-}
-
-public void setInput(String s) {
-  input = s;
-}
-
-public int getXlength() {
-  return xLength;
-}
-public int getYlength() {
-  return yLength;
-}
-
-public int getFoodX() {
-  return foodX;
-}
-
-public void setFoodX(int x) {
-  foodX = x;
-}
-
-public int getFoodY() {
-  return foodY;
-}
-
-public void setFoodY(int y) {
-  foodY = y;
-}
-
-public int getScore() {
-  return score;
-}
-
-public void setScore(int scoreToSet) {
-  score = scoreToSet;
-}
 }
