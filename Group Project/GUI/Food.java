ed2ca3ce00f220ef26ea7b7fc1596cbe3831ac90
Austin Levesque <32713540+austinlevesque@users.noreply.github.com>
2017-10-30 13:05:04 -0600

    Update Food.java

--- Group Project/GUI/Food.java
+++ Group Project/GUI/Food.java
@@ -1,6 +1,8 @@
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+import java.util.Random;
+
 public class Food{
 
 	Point topLeft = new Point(0,0);
@@ -15,6 +17,21 @@ public class Food{
 
 	public Point getTopLeft() {
 		return topLeft;
+	}
+	
+	public String getLoc(Point pointToCheck) {
+		String loc = ("("+pointToCheck.getXCoord()+","+pointToCheck.getYCoord()+")");
+		return loc;
+	}
+	
+	public Point newLoc() {
+		Random rand1 = new Random();
+		Random rand2 = new Random();
+		int r1 = rand1.nextInt(788);
+		int r2 = rand2.nextInt(780);
+		Point newPoint = new Point((r1-((r1%30))+8),r2-(r2%30));
+		System.out.println(getLoc(newPoint));
+		return newPoint;
 	}
 
 	public int getSize() {
