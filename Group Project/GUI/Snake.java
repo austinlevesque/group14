7b951d664aa71fa4bd9ddea5f6c4b75c0cde6104
Austin Levesque <32713540+austinlevesque@users.noreply.github.com>
2017-10-29 19:40:47 -0600

    Update Snake.java

--- Group Project/GUI/Snake.java
+++ Group Project/GUI/Snake.java
@@ -1,14 +1,42 @@
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-public class Snake extends Shape {
-	String direction = "down";
+public class Snake{
+	Point topLeft = new Point(0,0);
+	int size;
+	
+	  public Snake(Point snakeTopLeft, int snakeSize){
+			//super(snakeTopLeft, snakeSize);
+			topLeft = snakeTopLeft;
+			size = snakeSize;
+		}
 
-  public Snake(Point snakeTopLeft, int snakeSize){
-		super(snakeTopLeft, snakeSize);
-		topLeft = snakeTopLeft;
-		size = snakeSize;
-	}
+  //public abstract void draw(Graphics g);
+
+  public Point getTopLeft() {
+    return topLeft;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void moveDown(int amount) {
+    topLeft.moveDown(amount);
+  }
+
+  public void moveUp(int amount) {
+    topLeft.moveUp(amount);
+  }
+
+  public void moveLeft(int amount) {
+    topLeft.moveLeft(amount);
+  }
+
+  public void moveRight(int amount) {
+    topLeft.moveRight(amount);
+  }
+	String direction = "down";
 
 	public void setSize(int size2) {
 		size = size2;
