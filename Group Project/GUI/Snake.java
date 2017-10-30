b1f83837bef4bdbfc35286153e8b2162c042ef0f
Austin Levesque <32713540+austinlevesque@users.noreply.github.com>
2017-10-30 13:04:43 -0600

    Update Snake.java

--- Group Project/GUI/Snake.java
+++ Group Project/GUI/Snake.java
@@ -1,11 +1,14 @@
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 
 public class Snake{
 
 	Point topLeft = new Point(0,0);
 	int size;
+	int score = 0;
 	String direction = "down";
+	ArrayList<Point> tail = new ArrayList<Point>();
 	
 	public Snake(Point snakeTopLeft, int snakeSize) {
 		topLeft = snakeTopLeft;
@@ -16,6 +19,11 @@ public class Snake{
 
 	public Point getTopLeft() {
 		return topLeft;
+	}
+	
+	public String getLoc(Point pointToCheck) {
+		String loc = ("("+pointToCheck.getXCoord()+","+pointToCheck.getYCoord()+")");
+		return loc;
 	}
 
 	public int getSize() {
@@ -33,6 +41,28 @@ public class Snake{
 			topLeft.moveRight(amount);
 		}
 	}
+	
+	public void updateTail(Point prevPt){
+		tail.add(prevPt);
+		while(tail.size() > score){
+			tail.remove(0);
+		}
+	}
+	
+	/*
+	public void updateTail(int prevX, int prevY){
+		int tail = score;
+		tailX.add(prevX);
+		tailY.add(prevY);
+		if (tailX.size() > tail){
+		System.out.println("Tail end Position:"+tailX.get(0)+","+tailY.get(0));
+			while(tailX.size() > tail){
+				tailX.remove(0);
+				tailY.remove(0);
+			}
+		}
+	}
+	*/
 	
 	public void setSize(int size2) {
 		size = size2;
