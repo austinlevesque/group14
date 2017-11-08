01dd199180a03fc5fb3021dfb72f8ffd7f46f820
victorchu8 <victorchu1996@gmail.com>
2017-11-08 10:31:09 -0700

    Add files via upload

--- Group Project/GUI/Text.java
+++ Group Project/GUI/Text.java
@@ -5,9 +5,13 @@ import java.util.List;
 public class Text {
 
   private final int ARRAY_X = 10, ARRAY_Y = 10;
-  GameObject snake = new Snake(new Point (1,1), 0);
-  GameObject food = new Food(new Point(0,0), 0);
+  Snake snake = new Snake(new Point (1,1), 0);
+  Food food = new Food(new Point(0,0), 0);
   String input;
+
+
+  List<Integer> tailX = new ArrayList<Integer>();
+  List<Integer> tailY = new ArrayList<Integer>();
 
   public void grid() {
     System.out.println("Game Score: " + snake.score);
@@ -17,11 +21,55 @@ public class Text {
           arrayGrid[row][column] = ". ";
           arrayGrid[food.getTopLeft().getYCoord()][food.getTopLeft().getXCoord()] = "* ";
           arrayGrid[snake.getTopLeft().getYCoord()][snake.getTopLeft().getXCoord()] = "S ";
+          for(int j = snake.tail.size(); j>0; j--){
+              arrayGrid[snake.tail.get(j-1).getYCoord()][snake.tail.get(j-1).getXCoord()] = "s ";
+          }
           System.out.print(arrayGrid[row][column]);
+  /*
+          if (snake.tail.size() > 0){
+          arrayGrid[snake.tail.get(0).getYCoord()][snake.tail.get(0).getXCoord()] = "s ";
+          }
+          */
+//          for(int j = tailX.size(); j>0; j--){
+          //draws the tail and detects if the head has collided with the tail
+          /*
+            if(arrayGrid[(mainSnake.tailY.get(j-1))][mainSnake.tailX.get(j-1)] == "S "){
+            //if the head has collided with the tail
+              mainSnake.gameover = true;
+            }else{*/
+//              arrayGrid[tailY.get(j-1)][tailX.get(j-1)] = "s ";
+              //arrayGrid[snake.tail.get(j-1).getYCoord()][snake.tail.get(j-1).getXCoord()] = "s ";
+//          }
         }
         System.out.println();
     }
   }
+
+  /*
+	public void printGrid(){
+	  System.out.println("Game Score: " + getScore());
+	  String[][] arrayGrid = new String[mainSnake.getYlength()][mainSnake.getXlength()];
+	  for(int row = 0; row < mainSnake.getXlength(); row++){
+		for(int column = 0; column < mainSnake.getYlength(); column++){
+			arrayGrid[row][column] = ". ";
+			arrayGrid[mainSnake.getFoodY()][mainSnake.getFoodX()] = "* ";
+			arrayGrid[mainSnake.getY()][mainSnake.getX()] = "S ";
+			for(int j = mainSnake.tailX.size(); j>0; j--){
+			//draws the tail and detects if the head has collided with the tail
+				if(arrayGrid[(mainSnake.tailY.get(j-1))][mainSnake.tailX.get(j-1)] == "S "){
+				//if the head has collided with the tail
+					mainSnake.gameover = true;
+				}else{
+					arrayGrid[(mainSnake.tailY.get(j-1))][mainSnake.tailX.get(j-1)] = "s ";
+				}
+			}
+			System.out.print(arrayGrid[row][column]);
+		}
+		System.out.println();
+	  }
+	}
+   */
+
 
   public boolean eat(){
     boolean flag = false;
@@ -50,7 +98,7 @@ public class Text {
 
 		int moveX = snake.getTopLeft().getXCoord();
     int moveY = snake.getTopLeft().getYCoord();
-		//updateTail(getX(),getY());
+		snake.updateTail(snake.getTopLeft().getXCoord(), snake.getTopLeft().getYCoord());
 		//stores previous location data for the tail
 		if (input.equals("w")){
       moveY--;
@@ -83,6 +131,9 @@ public class Text {
     /*else{
 			this.running = false;
 		}*/
+		if (snake.tail.size() > 0){
+			System.out.println("Tail Position:"+snake.tail.get(0).getXCoord()+","+snake.tail.get(0).getYCoord());
+		}
 		System.out.println("Head Position:"+moveX+","+moveY);
 	}
 
