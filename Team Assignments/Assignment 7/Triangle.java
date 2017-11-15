3fdd8d630ff882c79c69f8c2ca6d8944f01ff81b
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2017-11-14 23:09:03 -0700

    Update Triangle.java

--- Team Assignments/Assignment 7/Triangle.java
+++ Team Assignments/Assignment 7/Triangle.java
@@ -1,17 +1,32 @@
-public class Triangle extends Shape {
+/*
+* Child class of Shape.  This class uses addLine method in Shape
+* to add lines in the shape of a triangle.
+* This class also calculates the area of the triangle
+*/
 
+public class Triangle extends Shape {
+	
+	/*
+	* Constructor that calls addLine method in Shape to add lines to the array list.
+	*/
 	public Triangle(Line line1, Line line2, Line line3) {
 		addLine(line1);
 		addLine(line2);
 		addLine(line3);
 	}
-
+	
+	/*
+	* This method calculates area of a triangle.
+	* Takes side lengths from a method in Shape and uses Heron's Formula 
+	* to calculate the area.
+	* @return double
+	*/
 	public double getArea() {
 		double s1 = getShape().get(0).length();
 		double s2 = getShape().get(1).length();
 		double s3 = getShape().get(2).length();
 		double s = (s1 + s2 + s3) / 2;
-    double area = Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
+    		double area = Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
 		return area;
 	}
 }
