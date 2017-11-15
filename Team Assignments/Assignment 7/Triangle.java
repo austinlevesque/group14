e6d56c421deb033f10bbdfc94ff7c9b7261450ad
victorchu8 <victorchu1996@gmail.com>
2017-11-14 20:12:35 -0700

    Add files via upload

--- Team Assignments/Assignment 7/Triangle.java
+++ Team Assignments/Assignment 7/Triangle.java
@@ -1,4 +1,3 @@
-
 public class Triangle extends Shape {
 
 	public Triangle(Line line1, Line line2, Line line3) {
@@ -7,7 +6,12 @@ public class Triangle extends Shape {
 		addLine(line3);
 	}
 
-	public double getArea(){
-		double s = 
+	public double getArea() {
+		double s1 = getShape().get(0).length();
+		double s2 = getShape().get(1).length();
+		double s3 = getShape().get(2).length();
+		double s = (s1 + s2 + s3) / 2;
+    double area = Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
+		return area;
 	}
 }
