af453369404f77454cfdb08be0cd4cc1dd2c3564
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2017-12-07 14:40:57 -0700

    Update GameBoard.java

--- Group Project/GameBoard.java
+++ Group Project/GameBoard.java
@@ -1,16 +1,23 @@
 package gameobjects;
 
+import fileio.HighscoreManager;
 import move.Point;
+import run.GUI;
+import run.GameOverScreen;
+import run.HighScore;
 
 public class GameBoard {
 
 	private static final int SHAPE_SIZE = 15;
-	private Point p1 = new Point(8, 30);
-	private Point p2 = new Point(98, 120);
-	private Point p3 = new Point(788, 780);
-	private Snake aSnake = new Snake(p1, SHAPE_SIZE);
-	private Snake bSnake = new Snake(p3, SHAPE_SIZE);
-	private Food aFood = new Food(p2, SHAPE_SIZE);
+	static int color1, color2, color3;
+	private Point p1;
+	private Point p2;
+	private Point p3;
+	private Snake aSnake;
+	private Snake bSnake;
+	private Food aFood;
+	String[] args = new String[] {""};
+	
 
 	public void guiUpdateSnake() {
 		aSnake.updateTail(p1.getXCoord(), p1.getYCoord());
@@ -18,8 +25,10 @@ public class GameBoard {
 		aSnake.moveDir(30);
 		bSnake.moveDir(30);
 		if (aSnake.getTailStrings().contains(aSnake.getLoc(p1))) {
+			GUI.disposeMethod();
 			System.out.println("Game over");
 			resetGame();
+			GameOverScreen.main(args);
 		}
 		if (aSnake.getLoc(aSnake.getTopLeft()).equals(aFood.getLoc(aFood.getTopLeft()))) {
 			aSnake.setScore(aSnake.getScore() + 1);
@@ -30,9 +39,9 @@ public class GameBoard {
 			aFood = new Food(aFood.newLoc(26), 15);
 		}
 	}
-
+	
 	public void resetGame() {
-		final int SHAPE_SIZE = 15;
+		//HighscoreManager.writeScore();
 		p1 = new Point(8, 30);
 		p2 = new Point(98, 120);
 		p3 = new Point(788, 780);
